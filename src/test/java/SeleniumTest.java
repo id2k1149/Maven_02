cd8240d02a6f34d5e33722f7002ab128237aad9b
MikePolyakov <id2k1149@gmail.com>
2020-04-07 22:51:36 +0300

    initial commit

---
+++ src/test/java/SeleniumTest.java
@@ -0,0 +1,16 @@
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+    @Test
+    public void BrowserAutomation()
+    {
+        System.out.println("BrowserAutomation");
+    }
+
+    @Test
+    public void elementsUI()
+    {
+        System.out.println("elementsUI");
+    }
+}
